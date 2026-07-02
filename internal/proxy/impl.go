005fb1c672b99c9d097328a3d23fa84103cb139b
Hieu Pham <hieu.pham@reddit.com>
2026-07-02 10:06:06 -0700

    RDDT: Add granular proxy search latency bucket metrics

--- internal/proxy/impl.go
+++ internal/proxy/impl.go
@@ -3113,6 +3113,13 @@ func (node *Proxy) search(ctx context.Context, request *milvuspb.SearchRequest, 
 		collectionName,
 	).Observe(float64(searchDur))
 
+	metrics.ProxySQLatencyGranular.WithLabelValues(
+		nodeID,
+		metrics.SearchLabel,
+		dbName,
+		collectionName,
+	).Observe(float64(searchDur))
+
 	metrics.ProxyCollectionSQLatency.WithLabelValues(
 		nodeID,
 		metrics.SearchLabel,
@@ -3338,6 +3345,13 @@ func (node *Proxy) hybridSearch(ctx context.Context, request *milvuspb.HybridSea
 
 	searchDur := tr.ElapseSpan().Milliseconds()
 	metrics.ProxySQLatency.WithLabelValues(
+		nodeID,
+		metrics.HybridSearchLabel,
+		dbName,
+		collectionName,
+	).Observe(float64(searchDur))
+
+	metrics.ProxySQLatencyGranular.WithLabelValues(
 		nodeID,
 		metrics.HybridSearchLabel,
 		dbName,
@@ -3795,6 +3809,13 @@ func (node *Proxy) query(ctx context.Context, qt *queryTask, sp trace.Span) (*mi
 		).Observe(float64(span.Milliseconds()))
 
 		metrics.ProxySQLatency.WithLabelValues(
+			strconv.FormatInt(paramtable.GetNodeID(), 10),
+			queryLabel,
+			request.GetDbName(),
+			request.GetCollectionName(),
+		).Observe(float64(tr.ElapseSpan().Milliseconds()))
+
+		metrics.ProxySQLatencyGranular.WithLabelValues(
 			strconv.FormatInt(paramtable.GetNodeID(), 10),
 			queryLabel,
 			request.GetDbName(),
